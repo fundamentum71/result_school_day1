78c8e55f759b16ce5fdc22b298af1aeedb8fa61f
Aleksandr <it.kucherov@gmail.com>
2023-02-01 08:35:42 +0300

    слайдер сделал функцией

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(i = 0) {
+	const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-	slide.addEventListener('click', () => {
-		clearActiveClasses();
-		slide.classList.add('active');
-	});
+	slides[i].classList.add('active');
+
+	for (const slide of slides) {
+		slide.addEventListener('click', () => {
+			clearActiveClasses();
+			slide.classList.add('active');
+		});
+	}
+
+	function clearActiveClasses() {
+		slides.forEach((slide) => {
+			slide.classList.remove('active');
+		});
+	}
 }
 
-function clearActiveClasses() {
-	slides.forEach((slide) => {
-		slide.classList.remove('active');
-	});
-}
+slidesPlugin();
